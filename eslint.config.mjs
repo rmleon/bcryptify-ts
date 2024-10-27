50e387988dda11894fb1e2b10a6fb70ad1f20be7
Ricardo <rmleon@users.noreply.github.com>
2024-10-26 22:31:01 -0400

    Cleaning lint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["src/**/*.ts"]},
+  {files: ["src/**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
